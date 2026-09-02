2038719115e0ebc59c1a5707035b64680eabf667
Andrew <napierandr@gmail.com>
2026-09-02 12:42:34 +0800

    feat: replaced emoji home icon with sleek minimal SVG outline house

--- components/navbar.js
+++ components/navbar.js
@@ -62,7 +62,10 @@ export class Navbar {
             </button>
           ` : `
             <a href="#/" class="nav-brand" title="Home">
-              <span class="nav-brand-logo">🏠</span>
+              <svg class="nav-home-icon" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round">
+                <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/>
+                <polyline points="9 22 9 12 15 12 15 22"/>
+              </svg>
             </a>
           `}
         </div>
