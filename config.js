1411c3a441b0a5bcb7f3966c660229d0907caab9
Andrew <napierandr@gmail.com>
2026-09-02 09:47:37 +0800

    feat: link live AWS Lambda Function URLs

--- config.js
+++ config.js
@@ -2,14 +2,13 @@
 
 export const CONFIG = {
   // Google OAuth 2.0 Web Client ID
-  // Replace with your generated Google Web Client ID from Google Cloud Console
   GOOGLE_CLIENT_ID: '719377736713-p1u6fp5epck9hjhhh9jeuopt9t8q1ebl.apps.googleusercontent.com',
 
-  // AWS Lambda Function URLs (populated upon SAM deployment)
+  // Live AWS Lambda Function URLs
   API_ENDPOINTS: {
-    profile: '',    // e.g. 'https://xxx.lambda-url.us-east-1.on.aws/'
-    languages: '',  // e.g. 'https://xxx.lambda-url.us-east-1.on.aws/'
-    decks: '',      // e.g. 'https://xxx.lambda-url.us-east-1.on.aws/'
-    words: ''       // e.g. 'https://xxx.lambda-url.us-east-1.on.aws/'
+    profile: 'https://u5tqayw4k5zkibq55bubr7oxuq0riljr.lambda-url.us-east-1.on.aws/',
+    languages: 'https://7cz2cnzcmdzowalvrfg76b2geu0gqmre.lambda-url.us-east-1.on.aws/',
+    decks: 'https://5rd6ogkgak4e6335w6sm46aw4e0gkciz.lambda-url.us-east-1.on.aws/',
+    words: 'https://q7aojvvzptfhse6ortbt2uh2ia0afszl.lambda-url.us-east-1.on.aws/'
   }
 };
